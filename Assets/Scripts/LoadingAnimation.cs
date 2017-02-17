d8ca532af10e94e26170b8b5dd6155f4e8ce6d49
jerry_000 <jerrysupertw@gmail.com>
2017-02-17 21:13:21 +0800

    add loading animation

---
+++ Assets/Scripts/LoadingAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LoadingAnimation : MonoBehaviour {
+
+	public Image[] loadingAnim; 
+	public Image loadingImage;
+	public int num = 0;
+
+	// Use this for initialization
+	void Start () {
+		loadingImage = GetComponent<Image> ();
+		loadingAnim = Resources.LoadAll<Image> ("loading");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		loadingImage = loadingAnim [num];
+		num++;
+		num %= 120;
+	}
+}
