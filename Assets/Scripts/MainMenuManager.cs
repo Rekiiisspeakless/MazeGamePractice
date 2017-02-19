3301081a31c6ea313418ece1d6d429cf29d0a459
jerry_000 <jerrysupertw@gmail.com>
2017-02-20 00:49:01 +0800

    add sound effect

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class MainMenuManager : MonoBehaviour {
 
+public class MainMenuManager : MonoBehaviour {
+	public AudioSource musicPlayer; 
+	public AudioClip buttonClick;
 	// Use this for initialization
 	void Start () {
-		
+		musicPlayer = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
@@ -16,10 +18,25 @@ public class MainMenuManager : MonoBehaviour {
 	}
 
 	public void StartGame(){
+		ButtonClick ();
 		LoadingScreenManager.LoadScene(1);
 	}
 
 	public void ExitGame(){
+		ButtonClick ();
 		Application.Quit ();
+	}
+
+	void AudioManagement(){
+		if (!musicPlayer.isPlaying) {
+			musicPlayer.Play ();
+		} 
+	}
+	void ButtonClick(){
+		musicPlayer.Stop ();
+		musicPlayer.clip = buttonClick;
+		musicPlayer.volume = 0.7f;
+		musicPlayer.loop = false;
+		musicPlayer.Play ();
 	}
 }
